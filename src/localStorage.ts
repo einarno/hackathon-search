60d78182d29d3cbc20f5687eae2197fab2a727ce
Audun Follegg <audun.follegg@ignite.no>
2023-08-24 13:48:21 +0200

    Add getReview

--- src/localStorage.ts
+++ src/localStorage.ts
@@ -1,12 +1,20 @@
-import { Review } from "./types"
+import { Review } from "./types";
 
 export const setRecipeReview = (recipeId: number, review: Review) => {
-  localStorage.setItem(recipeId.toString(), JSON.stringify(review))
-}
+  localStorage.setItem(recipeId.toString(), JSON.stringify(review));
+};
 
 export const getRecipeReviews = () => {
   return Object.entries(localStorage).map(([key, value]) => ({
     recipeId: key,
     review: JSON.parse(value),
-  }))
-}
+  }));
+};
+
+export const getRecipeReview = (recipeId: number): Review | null => {
+  const review = localStorage.getItem(recipeId.toString());
+  if (review) {
+    return JSON.parse(review);
+  }
+  return null;
+};
