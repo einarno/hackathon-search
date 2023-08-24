0d837ddd66598c48ca3b4cd9b7700139ffd0a771
Audun Follegg <audun.follegg@ignite.no>
2023-08-24 13:54:28 +0200

    Add Hit component

--- src/Components/Hit.tsx
+++ src/Components/Hit.tsx
@@ -1,21 +1,22 @@
 import { Recipe } from "../helpers";
-import { Hit } from "meilisearch";
-import { Rating, Stack } from "@mui/material";
+import { Rating } from "@mui/material";
 import { getRecipeReview } from "../localStorage";
+import { useNavigate } from "@tanstack/react-router";
 
 type HitProps = {
   recipe: Recipe;
 };
 export function Hit(props: HitProps) {
+  const navigate = useNavigate();
   const review = getRecipeReview(props.recipe.id);
-  const data = props.recipe;
+  const recipe = props.recipe;
   return (
     <>
-      <Stack direction="row" spacing={2}>
-        <h3>{data.name}</h3>
-        <p>{data.description}</p>
+      <div onClick={() => navigate({ to: `/recipes/${recipe.id}` })}>
+        <h3>{recipe.name}</h3>
+        <p>{recipe.description}</p>
         <Rating name="read-only" value={review?.rating} readOnly />
-      </Stack>
+      </div>
     </>
   );
 }
--- src/Components/Hits.tsx
+++ src/Components/Hits.tsx
@@ -1,28 +1,18 @@
 import { useHits } from "react-instantsearch";
 import { recipesSchema } from "../helpers";
-import { useNavigate } from "@tanstack/react-router";
-
-
+import { Hit } from "./Hit";
 
 export const Hits = () => {
-    const { hits, } = useHits()
-    const navigate = useNavigate()
-    return (
-        <ol >
-            {
-                // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                hits.map((hit) => {
-                    const recipe = recipesSchema.parse(hit)
-                    return (
-                        <li key={recipe.id} onClick={() => {
-                            navigate({ to: `/recipes/${recipe.id}` })
-                        }}>
-                            <h3>{recipe.name}</h3>
-                            <h4>{recipe.description}</h4>
-                        </li>
-                    )
-                })
-            }
-        </ol >
-    )
-}
+  const { hits } = useHits();
+  return (
+    <ol>
+      {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        hits.map((hit) => {
+          const recipe = recipesSchema.parse(hit);
+          return <Hit key={recipe.id} recipe={recipe} />;
+        })
+      }
+    </ol>
+  );
+};
