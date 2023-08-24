f2c148c67b7ca8e82da0963a204d235b8bd9c5e7
einarno <86827863+einarno@users.noreply.github.com>
2023-08-24 17:14:07 +0200

    Add more pictures

--- src/Components/RecipeCard.tsx
+++ src/Components/RecipeCard.tsx
@@ -3,6 +3,7 @@ import { Card, CardContent, Grid, Stack, Typography } from "@mui/material";
 import { Recipe } from "../helpers";
 import { createApi } from 'unsplash-js';
 import { Link } from "@tanstack/react-router";
+import { set } from "zod";
 
 
 const unsplash = createApi({
@@ -14,13 +15,26 @@ type Props = {
     recipe: Recipe;
 };
 
+function getRandomInt(max: number) {
+    return Math.floor(Math.random() * max);
+}
+
+const foodArray = [
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg/1920px-Good_Food_Display_-_NCI_Visuals_Online.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/b/b2/Traditional_fruitcake.jpg",
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Ginger_Bread_House_PC210088.JPG/1024px-Ginger_Bread_House_PC210088.JPG',
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Panettone_-_Nicolettone_2017_-_IMG_7085_%2831752542285%29.jpg/1920px-Panettone_-_Nicolettone_2017_-_IMG_7085_%2831752542285%29.jpg",
+
+]
+
 export const RecipeCard: React.FC<Props> = ({ recipe }) => {
-    const [image, setImage] = React.useState<string>("https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg/1920px-Good_Food_Display_-_NCI_Visuals_Online.jpg");
+    const [image, setImage] = React.useState<string | undefined>();
     useEffect(() => {
         if (!image) {
             unsplash.search.getPhotos({ query: recipe.name, page: 1, perPage: 1 }).then((result) => {
                 if (result.errors) {
                     // handle error here
+                    setImage(foodArray[getRandomInt(foodArray.length - 1)])
                     console.log('error occurred: ', result.errors[0]);
                 } else {
                     // handle success here
--- src/Components/RecipeList.tsx
+++ src/Components/RecipeList.tsx
@@ -1,7 +1,7 @@
 import { useHits } from "react-instantsearch";
 import { recipesSchema } from "../helpers";
 import { RecipeCard } from "./RecipeCard";
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 
 
 
@@ -9,6 +9,9 @@ export const RecipeList = () => {
     const { hits } = useHits();
     return (
         <Grid container spacing={2}>
+            <Grid item xs={12}>
+                <Typography variant="h2">Featured recipes </Typography>
+            </Grid>
             {
                 // eslint-disable-next-line @typescript-eslint/no-explicit-any
                 hits.map((hit) => {
