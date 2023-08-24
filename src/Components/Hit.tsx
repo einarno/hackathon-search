e05476cc1caf338a55754913172ea87124e77196
Audun Follegg <audun.follegg@ignite.no>
2023-08-24 14:41:24 +0200

    Add a tiny bit of styling

--- src/Components/Hit.tsx
+++ src/Components/Hit.tsx
@@ -1,10 +1,21 @@
 import { Recipe } from "../helpers";
-import { Rating, Stack, TextField } from "@mui/material";
+import { Rating, Stack } from "@mui/material";
+import CommentIcon from "@mui/icons-material/CommentRounded";
 import { getRecipeReview } from "../localStorage";
 import { useNavigate } from "@tanstack/react-router";
 
 type HitProps = {
   recipe: Recipe;
+};
+
+const style = {
+  border: "1px solid grey ",
+  "border-radius": "5px",
+  padding: "10px",
+  "&:hover": {
+    cursor: "pointer",
+    "background-color": "red",
+  },
 };
 export function Hit(props: HitProps) {
   const navigate = useNavigate();
@@ -12,14 +23,24 @@ export function Hit(props: HitProps) {
   const recipe = props.recipe;
   return (
     <>
-      <div onClick={() => navigate({ to: `/recipes/${recipe.id}` })}>
+      <div
+        onClick={() => navigate({ to: `/recipes/${recipe.id}` })}
+        className="search-item"
+        style={style}
+      >
         <Stack direction="row" spacing={2}>
           <Stack direction="column" spacing={2}>
             <h3>{recipe.name}</h3>
             <p>{recipe.description}</p>
           </Stack>
-          <Rating name="read-only" value={review?.rating} readOnly />
-          {review?.comment && <TextField value={review?.comment} />}
+          <Stack
+            direction="row"
+            spacing={2}
+            style={{ colorScheme: "light", alignItems: "center" }}
+          >
+            <Rating name="read-only" value={review?.rating} readOnly />
+            {<CommentIcon />}
+          </Stack>
         </Stack>
       </div>
     </>
