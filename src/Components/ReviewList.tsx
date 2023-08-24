8080a582c6792b2421922d113db7c4332d9b6198
Mathias Backsæther <mathias.backsaether@ignite.no>
2023-08-24 12:27:41 +0200

    update to table for reviews

--- src/Components/ReviewList.tsx
+++ src/Components/ReviewList.tsx
@@ -1,18 +1,50 @@
-import { List, ListItem, ListItemText } from "@mui/material"
-import { getRecipeReviews } from "../localStorage"
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import { getRecipeReviews } from "../localStorage";
+import { getRecipe } from "../helpers";
 
 export const ReviewList = () => {
-  const recipeReviews = getRecipeReviews()
+  const recipeReviews = getRecipeReviews();
   return (
-    <List>
-      {recipeReviews.map(({ recipeId, review }) => (
-        <ListItem key={review.id}>
-          <ListItemText primary={recipeId} />
-          <ListItemText primary={review.name} />
-          <ListItemText primary={review.rating} />
-          <ListItemText primary={review.comment} />
-        </ListItem>
-      ))}
-    </List>
-  )
-}
+    <TableContainer component={Paper}>
+      <Table
+        sx={{ minWidth: 650 }}
+        aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Recipe</TableCell>
+            <TableCell align="right">Name</TableCell>
+            <TableCell align="right">Rating&nbsp;(1-5)</TableCell>
+            <TableCell align="right">Comment</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {recipeReviews.map(({ recipeId, review }) => {
+            const recipe = getRecipe(recipeId);
+            return (
+              <TableRow
+                key={review.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+                <TableCell
+                  component="th"
+                  scope="row">
+                  {recipe.status === "success" ? recipe.recipe.name : "Unknown"}
+                </TableCell>
+                <TableCell align="right">{review.userName}</TableCell>
+                <TableCell align="right">{review.rating}</TableCell>
+                <TableCell align="right">{review.comment}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
