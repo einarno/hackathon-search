31569d88888eb77d0b19adb21f3a6ee3c5d14f96
einarno <86827863+einarno@users.noreply.github.com>
2023-08-24 17:03:58 +0200

    Remove unused import

--- src/Components/Hits.tsx
+++ src/Components/Hits.tsx
@@ -2,31 +2,30 @@ import { useHits } from "react-instantsearch";
 import { recipesSchema } from "../helpers";
 import { Hit } from "./Hit";
 import { Stack } from "@mui/material";
-import { useState } from "react";
 
 type Props = {
-  selected: number;
-  onSelected: (index: number) => void;
+    selected: number;
+    onSelected: (index: number) => void;
 };
 
 export const Hits = ({ selected, onSelected }: Props) => {
-  const { hits } = useHits();
-  return (
-    <Stack gap={2} py={2}>
-      {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        hits.map((hit, index) => {
-          const recipe = recipesSchema.parse(hit);
-          return (
-            <Hit
-              selected={index === selected}
-              onClick={onSelected}
-              key={recipe.id}
-              recipe={recipe}
-            />
-          );
-        })
-      }
-    </Stack>
-  );
+    const { hits } = useHits();
+    return (
+        <Stack gap={2} py={2}>
+            {
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                hits.map((hit, index) => {
+                    const recipe = recipesSchema.parse(hit);
+                    return (
+                        <Hit
+                            selected={index === selected}
+                            onClick={onSelected}
+                            key={recipe.id}
+                            recipe={recipe}
+                        />
+                    );
+                })
+            }
+        </Stack>
+    );
 };
